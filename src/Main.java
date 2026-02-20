c1e1f279d9921f42c57736ebb8e5f5283481a9e6
kanishkupadhyay1 <kanishk.upadhyay2006@gmail.com>
2026-02-20 19:19:39 +0530

    UC1: Add welcome message module

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,117 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//Use case 1 Entry point
+public class PalindromeCheckerApplication1 {
+    public static boolean PalindromeCheckerDeuqe(String Input){
+        Deque<Character> deque=new ArrayDeque<>();
+        for(char c: Input.toCharArray()){
+            deque.add(c);
         }
+        while(deque.size()>1){
+            if(deque.removeFirst()!=deque.removeLast()){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public static boolean PalindromeCheckerStackQueue(String Input){
+        Stack<Character> ReversedInput=new Stack<>();
+        Queue<Character> queue=new LinkedList<>();
+        for(char c: Input.toCharArray()){
+            ReversedInput.push(c);
+            queue.add(c);
+        }
+        while(!queue.isEmpty()){
+            if(queue.poll()!=ReversedInput.pop()){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean PalindromeCheckerStack(String Input){
+        Stack<Character> ReversedInput=new Stack<>();
+        for(char c: Input.toCharArray()){
+            ReversedInput.push(c);
+        }
+        for(char c: Input.toCharArray()){
+            if(c!=ReversedInput.pop()){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean PalindromeCheckerTwoPointer(String Input){
+
+        char[] chars=Input.toCharArray();
+        int start=0;
+        int end=Input.length()-1;
+
+        while (start < end) {
+            if(chars[start]!=chars[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static boolean PalindromeCheckerReverseString(String Input){
+        for(int count=Input.length()-1;count>=0;count--){
+            if(Input.charAt(count)!=Input.charAt(Input.length()-count-1)){
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public static boolean palindromeChecker(String input){
+        for(int count=0;count<input.length()/2;count++){
+            if(input.charAt(count)!=input.charAt(input.length()-count-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0.00.0\nSystem Initialized Successfully.");
+
+        //hardcoded string to check UC2
+        String Input=new String("madam");
+        System.out.println("Input Text: "+Input);
+        System.out.println("Is it Palindrome? : "+palindromeChecker(Input));
+
+        //Uc 3 reverse string method
+        System.out.println("Input Text: "+Input);
+        System.out.println("Is it Palindrome? : "+PalindromeCheckerReverseString(Input));
+
+        //Uc 4 Two Pointer string method
+        Input=new String("radar");
+        System.out.println("Input Text: "+Input);
+        System.out.println("Is it Palindrome? : "+PalindromeCheckerReverseString(Input));
+
+        //Uc 5 Stack string method
+        Input=new String("noon");
+        System.out.println("Input Text: "+Input);
+        System.out.println("Is it Palindrome? : "+PalindromeCheckerStack(Input));
+
+        //Uc 6 Stack Queue string method
+        Input=new String("civic");
+        System.out.println("Input Text: "+Input);
+        System.out.println("Is it Palindrome? : "+PalindromeCheckerStackQueue(Input));
+
+        //Uc 6  Deque string method
+        Input=new String("refer");
+        System.out.println("Input Text: "+Input);
+        System.out.println("Is it Palindrome? : "+PalindromeCheckerDeuqe(Input));
     }
 }
---
+++ src/PalindromeCheckerApplication.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+//Use case 1 Entry point
+public class PalindromeCheckerApplication {
+
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0.00.0\nSystem Initialized Successfully.");
+
+
+    }
+}
