1036abaac684a75136bcc7e40f6905d435403e75
kanishkupadhyay1 <kanishk.upadhyay2006@gmail.com>
2026-02-20 19:38:49 +0530

    UC4:Character Array Based Palindrome Check

--- src/PalindromeCheckerApplication.java
+++ src/PalindromeCheckerApplication.java
@@ -3,23 +3,28 @@ import java.util.*;
 // Entry point
 public class PalindromeCheckerApplication {
 
-    public static boolean PalindromeCheckerReverseString(String Input){
-        for(int count=Input.length()-1;count>=0;count--){
-            if(Input.charAt(count)!=Input.charAt(Input.length()-count-1)){
+    public static boolean PalindromeCheckerTwoPointer(String Input){
+
+        char[] chars=Input.toCharArray();
+        int start=0;
+        int end=Input.length()-1;
+
+        while (start < end) {
+            if(chars[start]!=chars[end]){
                 return false;
             }
+            start++;
+            end--;
         }
         return true;
-
     }
     public static void main(String[] args){
         System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0.00.0\nSystem Initialized Successfully.");
 
-        //Uc 3 reverse string method
-        String Input=new String("madam");
+        //Uc 4 Two Pointer string method
+        String Input=new String("radar");
         System.out.println("Input Text: "+Input);
-        System.out.println("Is it Palindrome? : "+PalindromeCheckerReverseString(Input));
-
+        System.out.println("Is it Palindrome? : "+PalindromeCheckerTwoPointer(Input));
 
     }
 }
