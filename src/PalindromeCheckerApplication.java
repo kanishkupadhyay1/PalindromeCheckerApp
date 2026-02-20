e8e68c07e6666c8e2df4dc947cc303ed6c286580
kanishkupadhyay1 <kanishk.upadhyay2006@gmail.com>
2026-02-20 19:54:12 +0530

    UC7:Deque-Based Optimized Palindrome Checker

--- src/PalindromeCheckerApplication.java
+++ src/PalindromeCheckerApplication.java
@@ -3,15 +3,13 @@ import java.util.*;
 // Entry point
 public class PalindromeCheckerApplication {
 
-    public static boolean PalindromeCheckerStackQueue(String Input){
-        Stack<Character> ReversedInput=new Stack<>();
-        Queue<Character> queue=new LinkedList<>();
+    public static boolean PalindromeCheckerDeuqe(String Input){
+        Deque<Character> deque=new ArrayDeque<>();
         for(char c: Input.toCharArray()){
-            ReversedInput.push(c);
-            queue.add(c);
+            deque.add(c);
         }
-        while(!queue.isEmpty()){
-            if(queue.poll()!=ReversedInput.pop()){
+        while(deque.size()>1){
+            if(deque.removeFirst()!=deque.removeLast()){
                 return false;
             }
         }
@@ -21,9 +19,9 @@ public class PalindromeCheckerApplication {
     public static void main(String[] args){
         System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0.00.0\nSystem Initialized Successfully.");
 
-        //Uc 6 Stack Queue string method
-        String Input=new String("civic");
+        //Uc 7  Deque string method
+        String Input=new String("refer");
         System.out.println("Input Text: "+Input);
-        System.out.println("Is it Palindrome? : "+PalindromeCheckerStackQueue(Input));
+        System.out.println("Is it Palindrome? : "+PalindromeCheckerDeuqe(Input));
     }
 }
