a63068422d71e5bc2bafc92b798ce31c09b28d82
kanishkupadhyay1 <kanishk.upadhyay2006@gmail.com>
2026-02-27 10:26:35 +0530

    UC10: Case-Insensitive & Space-Ignored Palindrome

--- src/PalindromeCheckerApplication.java
+++ src/PalindromeCheckerApplication.java
@@ -15,15 +15,30 @@ public class PalindromeCheckerApplication {
         if(Input.charAt(start)!=Input.charAt(end)){
             return false;
         }
-       return PalindromeCheckRecursive(Input,start+1,end-1);
+        return PalindromeCheckRecursive(Input,start+1,end-1);
+    }
+
+    //method to normalize the input
+    public static String normalize(String Input){
+        StringBuilder sb=new StringBuilder();
+
+        for(char ch : Input.toCharArray()){
+            if(ch!=' ') {
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
+        return sb.toString();
     }
 
     //main method
     public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
         System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0.00.0\nSystem Initialized Successfully.");
 
-        //Uc 7  Deque string method
-        String Input=new String("madam");
+        //Uc 9  Palindrome Check Recursive
+        System.out.println("Enter the string");
+        String Input=sc.nextLine();
+        Input=new String(normalize(Input));
         System.out.println("Input Text: "+Input);
         System.out.println("Is it Palindrome? : "+PalindromeCheckRecursive(Input,0,Input.length()-1));
     }
